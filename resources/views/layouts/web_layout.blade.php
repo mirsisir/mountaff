524fdebceca3a20e489802c4f88535129878360c
Thats Enam <surelastmail@gmail.com>
2022-10-17 16:11:19 +0600

    ls

--- resources/views/layouts/web_layout.blade.php
+++ resources/views/layouts/web_layout.blade.php
@@ -78,23 +78,6 @@
 </header><!-- End Header -->
 
 
-<!-- ======= Hero Section ======= -->
-<section id="hero" class="clearfix">
-    <div class="container d-flex h-100">
-        <div class="row justify-content-center align-self-center" data-aos="fade-up">
-            <div class="col-lg-6  intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-                <h2>MountApp Solutions<br>for Your <span>Business!</span></h2>
-                <div>
-                    <a href="#about" class="btn-get-started scrollto">Get Started</a>
-                </div>
-            </div>
-
-            <div class="col-lg-6  order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
-                <img style="   filter: grayscale(100%);" src="https://www.pngarts.com/files/18/Illustration-PNG-HQ-Picture.png" alt="" class="img-fluid">
-            </div>
-        </div>
-
-
 
 @yield('content')
 <!-- ======= Footer ======= -->
@@ -111,7 +94,6 @@
                         <div class="col-sm-6">
 
                             <div class="footer-info">
-
                                 <h3>MountApp</h3>
                                 <p>IMonetizeIt provides variety of special abilities to make your business more
                                     profitable.
--- resources/views/web/web_index.blade.php
+++ resources/views/web/web_index.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.web_layout')
 @section('content')
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="clearfix">
+    <section id="hero" class="clearfix mt-0">
         <div class="container d-flex h-100">
             <div class="row justify-content-center align-self-center" data-aos="fade-up">
-                <div class="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-                    <h2>Your Smartlink. Your CPA. Your ONE Network<br>for Your <span>Business!</span></h2>
+                <div class="col intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
+                    <h2 style="    text-shadow: 2px 1px 3px white;">Your Smartlink. Your CPA.<br> Your ONE Network<br>for Your <span>Business!</span></h2>
                     <div>
                         <a href="#about" class="btn-get-started scrollto">Creating trustful business</a>
                     </div>
                 </div>
 
-                <div class="col-lg-6 intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
-                    <img src="{{asset('web/assets/img/intro-img.svg')}}" alt="" class="img-fluid">
-                </div>
+
             </div>
 
         </div>
