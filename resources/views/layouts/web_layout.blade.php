b47273c7781fe9f5fafd0d54f1908e43147e33db
Thats Enam <surelastmail@gmail.com>
2022-10-19 16:01:26 +0600

    MountApp to MountAff

--- resources/views/layouts/web_layout.blade.php
+++ resources/views/layouts/web_layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>MountApp Bootstrap Template - Index</title>
+    <title>MountAff Bootstrap Template - Index</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -36,7 +36,7 @@
 <header id="header" class="fixed-top d-flex align-items-center header-transparent">
     <div class="container d-flex align-items-center">
 
-        <h1 class="logo me-auto"><a href="{{route('home')}}">MountApp</a></h1>
+        <h1 class="logo me-auto"><a href="{{route('home')}}">MountAff</a></h1>
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
@@ -93,7 +93,7 @@
                         <div class="col-sm-6">
 
                             <div class="footer-info">
-                                <h3>MountApp</h3>
+                                <h3>MountAff</h3>
                                 <p>IMonetizeIt provides variety of special abilities to make your business more
                                     profitable.
 
@@ -197,14 +197,14 @@
 
     <div class="container">
         <div class="copyright">
-            &copy; Copyright <strong>MountApp</strong>. All Rights Reserved
+            &copy; Copyright <strong>MountAff</strong>. All Rights Reserved
         </div>
         <div class="credits">
             <!--
             All the links in the footer should remain intact.
             You can delete the links only if you purchased the pro version.
             Licensing information: https://bootstrapmade.com/license/
-            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=MountApp
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=MountAff
           -->
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
