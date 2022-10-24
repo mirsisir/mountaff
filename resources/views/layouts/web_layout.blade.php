f9e556c66103b24ba1b9e81d4cf66d257d67584d
mirsisir <mirsisir@gmail.com>
2022-10-24 20:08:25 +0600

    sotial link add

--- resources/views/layouts/web_layout.blade.php
+++ resources/views/layouts/web_layout.blade.php
@@ -121,8 +121,7 @@
 
                             <div class="social-links">
                                 <a href="https://twitter.com/AffMount" class="twitter"><i class="bi bi-twitter"></i></a>
-                                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                            
                                 <a href="https://www.linkedin.com/company/mountaff" class="linkedin"><i class="bi bi-linkedin"></i></a>
                             </div>
 
